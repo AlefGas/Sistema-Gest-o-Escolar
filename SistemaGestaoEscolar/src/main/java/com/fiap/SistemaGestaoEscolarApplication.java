b2721cad690461118fa8c4480ed007f0c07f159c
Alef <lafitealef@gmail.com>
2024-04-30 22:20:07 -0300

    atualizaçoes

--- SistemaGestaoEscolar/src/main/java/com/fiap/SistemaGestaoEscolarApplication.java
+++ SistemaGestaoEscolar/src/main/java/com/fiap/SistemaGestaoEscolarApplication.java
@@ -2,8 +2,10 @@ package com.fiap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SistemaGestaoEscolarApplication {
 
 	public static void main(String[] args) {
