8e417d14aaafff5709fc3e8f4d565c73c43b100a
Alef <lafitealef@gmail.com>
2024-05-07 17:31:27 -0300

    Pageable para Aluno por email criado

--- SistemaGestaoEscolar/src/main/java/com/fiap/repository/TurmaRepository.java
+++ SistemaGestaoEscolar/src/main/java/com/fiap/repository/TurmaRepository.java
@@ -1,13 +1,13 @@
 package com.fiap.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import com.fiap.model.Turma;
 
 public interface TurmaRepository extends JpaRepository<Turma, Long> {
 
-    // Page<Aluno> findByNome(String nome, Pageable pageable);
+    // Page<Aluno> findBySemestre(String semestre, Pageable pageable);
 
     // Page<Turma> findByAlunosNome(String nome, Pageable pageable);
 
