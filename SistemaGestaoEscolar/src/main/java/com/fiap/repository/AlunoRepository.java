d7320cc6e81b270edc6b8ca491cb73969478b939
Alef <lafitealef@gmail.com>
2024-05-07 17:32:10 -0300

    Pageable Aluno by Email criado

--- SistemaGestaoEscolar/src/main/java/com/fiap/repository/AlunoRepository.java
+++ SistemaGestaoEscolar/src/main/java/com/fiap/repository/AlunoRepository.java
@@ -1,12 +1,15 @@
 package com.fiap.repository;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fiap.model.Aluno;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 
-    
+    Page<Aluno> findByEmail(String email, Pageable pageable);
 
     
 }
